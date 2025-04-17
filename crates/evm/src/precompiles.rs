5ff9056aca8bbeeabf1d82d287521033ed0246d4
Matthias Seitz <matthias.seitz@outlook.de>
2025-04-17 17:47:18 +0200

    wip

--- crates/evm/src/precompiles.rs
+++ crates/evm/src/precompiles.rs
@@ -1,9 +1,15 @@
 //! Helpers for dealing with Precompiles.
 
-use alloc::borrow::Cow;
-use alloy_primitives::Address;
-use revm::{handler::EthPrecompiles, precompile::Precompiles, primitives::hardfork::SpecId};
-use revm::precompile::PrecompileFn;
+use alloc::{borrow::Cow, sync::Arc};
+use alloy_primitives::{
+    map::{HashMap, HashSet},
+    Address, Bytes,
+};
+use revm::{
+    handler::EthPrecompiles,
+    precompile::{PrecompileFn, PrecompileResult, Precompiles},
+    primitives::hardfork::SpecId,
+};
 
 /// A set of Precompiles according to a spec.
 #[derive(Debug, Clone)]
@@ -11,42 +17,43 @@ pub struct SpecPrecompiles<Spec> {
     /// The configured precompiles.
     ///
     /// This is a cow so that this can be modified on demand.
-    precompiles: Cow<'static, Precompiles>,
+    precompiles: PrecompilesMap,
     /// The spec these precompiles belong to.
     spec: Spec,
 }
 
-
 impl<Spec> SpecPrecompiles<Spec> {
     /// Creates the [`SpecPrecompiles`] from a static reference.
     pub const fn from_static(precompiles: &'static Precompiles, spec: Spec) -> Self {
-        Self { precompiles: Cow::Borrowed(precompiles), spec }
+        // Self { precompiles: Cow::Borrowed(precompiles), spec }
+        todo!()
     }
 
     /// Creates a new set of precompiles for that spec.
     pub fn new(precompiles: Cow<'static, Precompiles>, spec: Spec) -> Self {
-        Self { precompiles, spec }
+        todo!()
     }
-    
+
     /// Returns the configured precompiles
     pub fn precompiles(&self) -> &Precompiles {
-        &self.precompiles
+        todo!()
     }
-    
+
     /// Returns mutable access to the precompiles.
     pub fn precompiles_mut(&mut self) -> &mut Precompiles {
-        self.precompiles.to_mut()
+        todo!()
     }
-    
+
     /// Returns the configured spec.
     pub const fn spec(&self) -> &Spec {
         &self.spec
     }
-    
-    pub fn map_precompile<F>(&mut self, address: &Address, f: F) where F: Fn(PrecompileFn) -> PrecompileFn {
-        if let Some(precompile) = self.precompiles_mut().get(address) {
-            
-        }
+
+    pub fn map_precompile<F>(&mut self, address: &Address, f: F)
+    where
+        F: Fn(PrecompileFn) -> PrecompileFn,
+    {
+        if let Some(precompile) = self.precompiles_mut().get(address) {}
     }
 }
 
@@ -54,4 +61,24 @@ impl From<EthPrecompiles> for SpecPrecompiles<SpecId> {
     fn from(value: EthPrecompiles) -> Self {
         Self::from_static(value.precompiles, value.spec)
     }
+}
+
+enum PrecompilesMap {
+    Builtin(Cow<'static, Precompiles>),
+    Dynamic(PrecompilesMut),
+}
+
+#[derive(Clone)]
+pub struct DynPrecompile(pub(crate) Arc<dyn Precompile>);
+
+#[derive(Clone, Default)]
+struct PrecompilesMut {
+    /// Precompiles
+    inner: HashMap<Address, DynPrecompile>,
+    /// Addresses of precompile
+    addresses: HashSet<Address>,
+}
+
+pub trait Precompile {
+    fn call(&self, data: &Bytes, gas: u64) -> PrecompileResult;
 }
