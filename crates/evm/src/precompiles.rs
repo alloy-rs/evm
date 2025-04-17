734b3ee53bc6e54f343c0b7104e8352168004eea
Matthias Seitz <matthias.seitz@outlook.de>
2025-04-17 17:18:15 +0200

    wip

--- crates/evm/src/precompiles.rs
+++ crates/evm/src/precompiles.rs
@@ -1,7 +1,9 @@
 //! Helpers for dealing with Precompiles.
 
 use alloc::borrow::Cow;
+use alloy_primitives::Address;
 use revm::{handler::EthPrecompiles, precompile::Precompiles, primitives::hardfork::SpecId};
+use revm::precompile::PrecompileFn;
 
 /// A set of Precompiles according to a spec.
 #[derive(Debug, Clone)]
@@ -14,6 +16,7 @@ pub struct SpecPrecompiles<Spec> {
     spec: Spec,
 }
 
+
 impl<Spec> SpecPrecompiles<Spec> {
     /// Creates the [`SpecPrecompiles`] from a static reference.
     pub const fn from_static(precompiles: &'static Precompiles, spec: Spec) -> Self {
@@ -23,6 +26,27 @@ impl<Spec> SpecPrecompiles<Spec> {
     /// Creates a new set of precompiles for that spec.
     pub fn new(precompiles: Cow<'static, Precompiles>, spec: Spec) -> Self {
         Self { precompiles, spec }
+    }
+    
+    /// Returns the configured precompiles
+    pub fn precompiles(&self) -> &Precompiles {
+        &self.precompiles
+    }
+    
+    /// Returns mutable access to the precompiles.
+    pub fn precompiles_mut(&mut self) -> &mut Precompiles {
+        self.precompiles.to_mut()
+    }
+    
+    /// Returns the configured spec.
+    pub const fn spec(&self) -> &Spec {
+        &self.spec
+    }
+    
+    pub fn map_precompile<F>(&mut self, address: &Address, f: F) where F: Fn(PrecompileFn) -> PrecompileFn {
+        if let Some(precompile) = self.precompiles_mut().get(address) {
+            
+        }
     }
 }
 
