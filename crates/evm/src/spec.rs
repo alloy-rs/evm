d10c9dcc71770fdbd27b260ce8881a3ca799189b
Roman Hodulák <roman.hodulak@polyglot-software.com>
2025-09-17 00:06:08 +0200

    refactor: Create `SpecId` based on trait implemented for any `EthereumHardforks`

--- crates/evm/src/spec.rs
+++ crates/evm/src/spec.rs
@@ -1,53 +1,82 @@
 use alloy_consensus::BlockHeader;
 use alloy_hardforks::EthereumHardforks;
+use alloy_primitives::{BlockNumber, BlockTimestamp};
 use revm::primitives::hardfork::SpecId;
 
-/// Map the latest active hardfork at the given header to a [`SpecId`].
-pub fn spec<C, H>(chain_spec: &C, header: &H) -> SpecId
-where
-    C: EthereumHardforks,
-    H: BlockHeader,
-{
-    spec_by_timestamp_and_block_number(chain_spec, header.timestamp(), header.number())
+/// Map the latest active hardfork to a [`SpecId`].
+///
+/// The latest active hardfork is determined based on identifying block header attributes. There are
+/// several methods to use:
+/// * [`spec_for_header()`] to create [`SpecId`] based on a [`BlockHeader`].
+/// * [`spec_at_timestamp()`] to create [`SpecId`] based on a [`BlockTimestamp`] if possible.
+/// * [`spec_at_height()`] to create [`SpecId`] based on a [`BlockNumber`] if possible.
+/// * [`spec_at()`] to create [`SpecId`] based on all possible block identifiers used for hardfork
+///   activation.
+///
+/// It is not intended for this trait to be implemented manually. Instead, any [`EthereumHardforks`]
+/// gets `SpecMap` implementation automatically thanks to a blanket implementation.
+///
+/// [`spec_for_header()`]: Self::spec_for_header
+/// [`spec_at_timestamp()`]: Self::spec_at_timestamp
+/// [`spec_at_height()`]: Self::spec_at_height
+/// [`spec_at()`]: Self::spec_at
+pub trait SpecMap {
+    /// Map the latest active hardfork for `header` to a [`SpecId`].
+    fn spec_for_header(&self, header: &impl BlockHeader) -> SpecId {
+        self.spec_at(header.timestamp(), header.number())
+    }
+
+    /// Map the latest active hardfork at `timestamp` to a [`SpecId`].
+    fn spec_at_timestamp(&self, timestamp: BlockTimestamp) -> Option<SpecId>;
+
+    /// Map the latest active hardfork at `height` to a [`SpecId`].
+    fn spec_at_height(&self, height: BlockNumber) -> Option<SpecId>;
+
+    /// Map the latest active hardfork at `timestamp` or `height` to a [`SpecId`].
+    fn spec_at(&self, timestamp: BlockTimestamp, height: BlockNumber) -> SpecId {
+        self.spec_at_timestamp(timestamp)
+            .or_else(|| self.spec_at_height(height))
+            .unwrap_or(SpecId::FRONTIER)
+    }
 }
 
-/// Map the latest active hardfork at the given timestamp or block number to a [`SpecId`].
-pub fn spec_by_timestamp_and_block_number<C>(
-    chain_spec: &C,
-    timestamp: u64,
-    block_number: u64,
-) -> SpecId
-where
-    C: EthereumHardforks,
-{
-    if chain_spec.is_osaka_active_at_timestamp(timestamp) {
-        SpecId::OSAKA
-    } else if chain_spec.is_prague_active_at_timestamp(timestamp) {
-        SpecId::PRAGUE
-    } else if chain_spec.is_cancun_active_at_timestamp(timestamp) {
-        SpecId::CANCUN
-    } else if chain_spec.is_shanghai_active_at_timestamp(timestamp) {
-        SpecId::SHANGHAI
-    } else if chain_spec.is_paris_active_at_block(block_number) {
-        SpecId::MERGE
-    } else if chain_spec.is_london_active_at_block(block_number) {
-        SpecId::LONDON
-    } else if chain_spec.is_berlin_active_at_block(block_number) {
-        SpecId::BERLIN
-    } else if chain_spec.is_istanbul_active_at_block(block_number) {
-        SpecId::ISTANBUL
-    } else if chain_spec.is_petersburg_active_at_block(block_number) {
-        SpecId::PETERSBURG
-    } else if chain_spec.is_byzantium_active_at_block(block_number) {
-        SpecId::BYZANTIUM
-    } else if chain_spec.is_spurious_dragon_active_at_block(block_number) {
-        SpecId::SPURIOUS_DRAGON
-    } else if chain_spec.is_tangerine_whistle_active_at_block(block_number) {
-        SpecId::TANGERINE
-    } else if chain_spec.is_homestead_active_at_block(block_number) {
-        SpecId::HOMESTEAD
-    } else {
-        SpecId::FRONTIER
+impl<C: EthereumHardforks> SpecMap for C {
+    fn spec_at_timestamp(&self, timestamp: BlockTimestamp) -> Option<SpecId> {
+        Some(if self.is_osaka_active_at_timestamp(timestamp) {
+            SpecId::OSAKA
+        } else if self.is_prague_active_at_timestamp(timestamp) {
+            SpecId::PRAGUE
+        } else if self.is_cancun_active_at_timestamp(timestamp) {
+            SpecId::CANCUN
+        } else if self.is_shanghai_active_at_timestamp(timestamp) {
+            SpecId::SHANGHAI
+        } else {
+            return None;
+        })
+    }
+
+    fn spec_at_height(&self, height: BlockNumber) -> Option<SpecId> {
+        Some(if self.is_paris_active_at_block(height) {
+            SpecId::MERGE
+        } else if self.is_london_active_at_block(height) {
+            SpecId::LONDON
+        } else if self.is_berlin_active_at_block(height) {
+            SpecId::BERLIN
+        } else if self.is_istanbul_active_at_block(height) {
+            SpecId::ISTANBUL
+        } else if self.is_petersburg_active_at_block(height) {
+            SpecId::PETERSBURG
+        } else if self.is_byzantium_active_at_block(height) {
+            SpecId::BYZANTIUM
+        } else if self.is_spurious_dragon_active_at_block(height) {
+            SpecId::SPURIOUS_DRAGON
+        } else if self.is_tangerine_whistle_active_at_block(height) {
+            SpecId::TANGERINE
+        } else if self.is_homestead_active_at_block(height) {
+            SpecId::HOMESTEAD
+        } else {
+            return None;
+        })
     }
 }
 
@@ -56,8 +85,17 @@ mod tests {
     use super::*;
     use crate::eth::spec::EthSpec;
     use alloy_consensus::Header;
-    use alloy_hardforks::{EthereumHardfork, ForkCondition};
-    use alloy_primitives::{BlockNumber, BlockTimestamp, U256};
+    use alloy_hardforks::{
+        mainnet::{
+            MAINNET_BERLIN_BLOCK, MAINNET_BYZANTIUM_BLOCK, MAINNET_CANCUN_TIMESTAMP,
+            MAINNET_FRONTIER_BLOCK, MAINNET_HOMESTEAD_BLOCK, MAINNET_ISTANBUL_BLOCK,
+            MAINNET_LONDON_BLOCK, MAINNET_PARIS_BLOCK, MAINNET_PETERSBURG_BLOCK,
+            MAINNET_PRAGUE_TIMESTAMP, MAINNET_SHANGHAI_TIMESTAMP, MAINNET_SPURIOUS_DRAGON_BLOCK,
+            MAINNET_TANGERINE_BLOCK,
+        },
+        EthereumHardfork, ForkCondition,
+    };
+    use alloy_primitives::{BlockNumber, BlockTimestamp};
 
     struct FakeHardfork {
         fork: EthereumHardfork,
@@ -65,6 +103,14 @@ mod tests {
     }
 
     impl FakeHardfork {
+        fn osaka() -> Self {
+            Self::from_timestamp_zero(EthereumHardfork::Osaka)
+        }
+
+        fn prague() -> Self {
+            Self::from_timestamp_zero(EthereumHardfork::Prague)
+        }
+
         fn cancun() -> Self {
             Self::from_timestamp_zero(EthereumHardfork::Cancun)
         }
@@ -124,19 +170,8 @@ mod tests {
         }
     }
 
-    #[test_case::test_case(FakeHardfork::cancun(), 0, SpecId::CANCUN; "Cancun")]
-    #[test_case::test_case(FakeHardfork::shanghai(), 0, SpecId::SHANGHAI; "Shanghai")]
-    #[test_case::test_case(EthSpec::mainnet(), 15537394, SpecId::MERGE; "Paris")]
-    fn test_spec_maps_timestamp_successfully(
-        fork: impl EthereumHardforks,
-        block_number: BlockNumber,
-        expected_spec: SpecId,
-    ) {
-        let actual_spec = spec_by_timestamp_and_block_number(&fork, 0, block_number);
-
-        assert_eq!(actual_spec, expected_spec);
-    }
-
+    #[test_case::test_case(FakeHardfork::osaka(), SpecId::OSAKA; "Osaka")]
+    #[test_case::test_case(FakeHardfork::prague(), SpecId::PRAGUE; "Prague")]
     #[test_case::test_case(FakeHardfork::cancun(), SpecId::CANCUN; "Cancun")]
     #[test_case::test_case(FakeHardfork::shanghai(), SpecId::SHANGHAI; "Shanghai")]
     #[test_case::test_case(FakeHardfork::paris(), SpecId::MERGE; "Merge")]
@@ -148,31 +183,33 @@ mod tests {
     #[test_case::test_case(FakeHardfork::homestead(), SpecId::HOMESTEAD; "Homestead")]
     #[test_case::test_case(FakeHardfork::frontier(), SpecId::FRONTIER; "Frontier")]
     fn test_spec_maps_hardfork_successfully(fork: impl EthereumHardforks, expected_spec: SpecId) {
-        let actual_spec = spec(&fork, &Header::default());
+        let header = Header::default();
+        let actual_spec = fork.spec_for_header(&header);
 
         assert_eq!(actual_spec, expected_spec);
     }
 
-    #[test_case::test_case(1710338135, 0, U256::ZERO, SpecId::CANCUN; "Cancun")]
-    #[test_case::test_case(1681338455, 0, U256::ZERO, SpecId::SHANGHAI; "Shanghai")]
-    #[test_case::test_case(0, 15537394, U256::from(10_u128), SpecId::MERGE; "Merge")]
-    #[test_case::test_case(0, 15537394 - 10, U256::ZERO, SpecId::LONDON; "London")]
-    #[test_case::test_case(0, 12244000 + 10, U256::ZERO, SpecId::BERLIN; "Berlin")]
-    #[test_case::test_case(0, 12244000 - 10, U256::ZERO, SpecId::ISTANBUL; "Istanbul")]
-    #[test_case::test_case(0, 7280000 + 10, U256::ZERO, SpecId::PETERSBURG; "Petersburg")]
-    #[test_case::test_case(0, 7280000 - 10, U256::ZERO, SpecId::BYZANTIUM; "Byzantium")]
-    #[test_case::test_case(0, 2675000 + 10, U256::ZERO, SpecId::SPURIOUS_DRAGON; "Spurious dragon")]
-    #[test_case::test_case(0, 2675000 - 10, U256::ZERO, SpecId::TANGERINE; "Tangerine")]
-    #[test_case::test_case(0, 1150000 + 10, U256::ZERO, SpecId::HOMESTEAD; "Homestead")]
-    #[test_case::test_case(0, 1150000 - 10, U256::ZERO, SpecId::FRONTIER; "Frontier")]
-    fn test_spec_maps_header_successfully(
+    #[test_case::test_case(MAINNET_PRAGUE_TIMESTAMP, 0, SpecId::PRAGUE; "Prague")]
+    #[test_case::test_case(MAINNET_CANCUN_TIMESTAMP, 0, SpecId::CANCUN; "Cancun")]
+    #[test_case::test_case(MAINNET_SHANGHAI_TIMESTAMP, 0, SpecId::SHANGHAI; "Shanghai")]
+    #[test_case::test_case(0, MAINNET_PARIS_BLOCK, SpecId::MERGE; "Merge")]
+    #[test_case::test_case(0, MAINNET_LONDON_BLOCK, SpecId::LONDON; "London")]
+    #[test_case::test_case(0, MAINNET_BERLIN_BLOCK, SpecId::BERLIN; "Berlin")]
+    #[test_case::test_case(0, MAINNET_ISTANBUL_BLOCK, SpecId::ISTANBUL; "Istanbul")]
+    #[test_case::test_case(0, MAINNET_PETERSBURG_BLOCK, SpecId::PETERSBURG; "Petersburg")]
+    #[test_case::test_case(0, MAINNET_BYZANTIUM_BLOCK, SpecId::BYZANTIUM; "Byzantium")]
+    #[test_case::test_case(0, MAINNET_SPURIOUS_DRAGON_BLOCK, SpecId::SPURIOUS_DRAGON; "Spurious dragon")]
+    #[test_case::test_case(0, MAINNET_TANGERINE_BLOCK, SpecId::TANGERINE; "Tangerine")]
+    #[test_case::test_case(0, MAINNET_HOMESTEAD_BLOCK, SpecId::HOMESTEAD; "Homestead")]
+    #[test_case::test_case(0, MAINNET_FRONTIER_BLOCK, SpecId::FRONTIER; "Frontier")]
+    fn test_eth_spec_maps_hardfork_successfully(
         timestamp: BlockTimestamp,
         number: BlockNumber,
-        difficulty: U256,
         expected_spec: SpecId,
     ) {
-        let header = Header { timestamp, number, difficulty, ..Default::default() };
-        let actual_spec = spec(&EthSpec::mainnet(), &header);
+        let fork = EthSpec::mainnet();
+        let header = Header { timestamp, number, ..Default::default() };
+        let actual_spec = fork.spec_for_header(&header);
 
         assert_eq!(actual_spec, expected_spec);
     }
