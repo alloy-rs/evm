0ac39a5cd7b472452b660586701ad919ebdec409
Roman Hodulák <roman.hodulak@polyglot-software.com>
2025-09-19 14:56:07 +0200

    traits wip

--- crates/evm/src/env.rs
+++ crates/evm/src/env.rs
@@ -19,26 +19,6 @@ pub struct EvmEnv<Spec = SpecId> {
     pub block_env: BlockEnv,
 }
 
-impl EvmEnv<SpecId> {
-    /// Create a new `EvmEnv` from a block `header`, `chain_id`, chain `spec` and optional
-    /// `blob_params`.
-    ///
-    /// # Arguments
-    ///
-    /// * `header` - The block to make the env out of.
-    /// * `chain_spec` - The chain hardfork description, must implement [`EthereumHardforks`].
-    /// * `chain_id` - The chain identifier.
-    /// * `blob_params` - Optional parameters that sets limits on gas and count for blobs.
-    pub fn eth_for_block(
-        header: impl BlockHeader,
-        chain_spec: impl EthereumHardforks,
-        chain_id: ChainId,
-        blob_params: Option<BlobParams>,
-    ) -> Self {
-        Self::for_block(header, chain_spec, chain_id, blob_params, crate::spec)
-    }
-}
-
 impl<Spec> EvmEnv<Spec> {
     /// Create a new `EvmEnv` from a block `header`, `chain_id`, chain `spec` and optional
     /// `blob_params`.
@@ -76,7 +56,7 @@ impl<Spec> EvmEnv<Spec> {
         }
 
         // derive the EIP-4844 blob fees from the header's `excess_blob_gas` and the current
-        // blobparams
+        // blob-params
         let blob_excess_gas_and_price =
             header.excess_blob_gas().zip(blob_params).map(|(excess_blob_gas, params)| {
                 let blob_gasprice = params.calc_blob_fee(excess_blob_gas);
@@ -261,7 +241,7 @@ mod tests {
         let chain_id = 2;
         let spec = EthSpec::mainnet();
         let blob_params = None;
-        let actual_evm_env = EvmEnv::eth_for_block(header, spec, chain_id, blob_params);
+        let actual_evm_env = EvmEnv::for_block(header, spec, chain_id, blob_params, crate::spec);
 
         assert_eq!(actual_evm_env, expected_evm_env);
     }
--- crates/op-evm/src/env.rs
+++ crates/op-evm/src/env.rs
@@ -1,13 +1,15 @@
+use crate::spec;
 use alloy_consensus::BlockHeader;
 use alloy_eips::eip7840::BlobParams;
-use alloy_evm::EvmEnv;
-use alloy_op_hardforks::OpHardforks;
+use alloy_evm::{eth::spec::EthSpec, EvmEnv};
+use alloy_hardforks::{EthereumChainHardforks, EthereumHardforks};
+use alloy_op_hardforks::OpChainHardforks;
 use alloy_primitives::ChainId;
 use op_revm::OpSpecId;
+use revm::primitives::hardfork::SpecId;
 
-/// An extension intended for [`EvmEnv`] that adds an extra constructor that creates an instance
-/// with [`OpSpecId`].
-pub trait ExtOpEvmEnv {
+/// An extension intended for [`EvmEnv`].
+pub trait ExtEvmEnv<Spec> {
     /// Create a new `EvmEnv` from a block `header`, `chain_id`, chain `spec` and optional
     /// `blob_params`.
     ///
@@ -15,32 +17,67 @@ pub trait ExtOpEvmEnv {
     ///
     /// * `header` - The block to make the env out of.
     /// * `chain_id` - The chain identifier.
-    /// * `spec` - The chain hardfork description, must implement [`OpHardforks`].
+    /// * `spec` - The chain hardfork description, must implement [`EthereumHardforks`] and
+    ///   [`DeriveSpec`].
     /// * `blob_params` - Optional parameters that sets limits on gas and count for blobs.
-    fn op_for_block(
+    fn for_block(
         header: impl BlockHeader,
-        chain_spec: impl OpHardforks,
+        chain_spec: impl EthereumHardforks + DeriveSpec<SpecId = Spec>,
         chain_id: ChainId,
         blob_params: Option<BlobParams>,
     ) -> Self;
 }
 
-impl ExtOpEvmEnv for EvmEnv<OpSpecId> {
+impl<Spec> ExtEvmEnv<Spec> for EvmEnv<Spec> {
     /// Create a new `EvmEnv` from a block `header`, `chain_id`, `chain_spec` and optional
     /// `blob_params`.
     ///
     /// # Arguments
     ///
     /// * `header` - The block to make the env out of.
-    /// * `chain_spec` - The chain hardfork description, must implement [`OpHardforks`].
+    /// * `chain_spec` - The chain hardfork description, must implement [`EthereumHardforks`] and
+    ///   [`DeriveSpec`].
     /// * `chain_id` - The chain identifier.
     /// * `blob_params` - Optional parameters that sets limits on gas and count for blobs.
-    fn op_for_block(
+    fn for_block(
         header: impl BlockHeader,
-        chain_spec: impl OpHardforks,
+        chain_spec: impl EthereumHardforks + DeriveSpec<SpecId = Spec>,
         chain_id: ChainId,
         blob_params: Option<BlobParams>,
     ) -> Self {
-        Self::for_block(header, chain_spec, chain_id, blob_params, |c, h| crate::spec(c, h))
+        Self::for_block(header, chain_spec, chain_id, blob_params, |c, h| c.for_block(h))
+    }
+}
+
+/// Map the latest active hardfork at the given header to a [`Self::SpecId`].
+pub trait DeriveSpec {
+    /// An associated spec ID type for a hardfork chain specification.
+    type SpecId;
+
+    /// Maps the latest active hardfork at the given header to a [`Self::SpecId`].
+    fn for_block(&self, header: impl BlockHeader) -> Self::SpecId;
+}
+
+impl DeriveSpec for EthSpec {
+    type SpecId = SpecId;
+
+    fn for_block(&self, header: impl BlockHeader) -> Self::SpecId {
+        alloy_evm::spec(self, &header)
+    }
+}
+
+impl DeriveSpec for EthereumChainHardforks {
+    type SpecId = SpecId;
+
+    fn for_block(&self, header: impl BlockHeader) -> Self::SpecId {
+        alloy_evm::spec(self, &header)
+    }
+}
+
+impl DeriveSpec for OpChainHardforks {
+    type SpecId = OpSpecId;
+
+    fn for_block(&self, header: impl BlockHeader) -> Self::SpecId {
+        spec(self, &header)
     }
 }
